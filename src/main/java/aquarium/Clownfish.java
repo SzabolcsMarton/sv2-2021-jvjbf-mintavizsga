8fea0461c58031e06e4bfb4922e48a3da0a4f6c2
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-12 19:28:04 +0100

    end of time commit

---
+++ src/main/java/aquarium/Clownfish.java
@@ -0,0 +1,19 @@
+package aquarium;
+
+public class Clownfish extends Fish {
+
+    public Clownfish(String name, int weight, String color) {
+        super(name, weight, color);
+    }
+
+    @Override
+    public boolean hasMemoryLoss() {
+        return false;
+    }
+
+    @Override
+    public void feed() {
+        this.weight += 1;
+
+    }
+}
---
+++ src/main/java/aquarium/Fish.java
@@ -0,0 +1,37 @@
+package aquarium;
+
+public abstract class Fish {
+
+    private String name;
+    protected int weight;
+    private String color;
+
+
+    public Fish(String name, int weight, String color) {
+        this.name = name;
+        this.weight = weight;
+        this.color = color;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract boolean hasMemoryLoss();
+
+    public abstract void feed();
+
+    public String getStatus(){
+        return name + ", weight: " + weight + ", color: " + ", short-term memory loss: " + hasMemoryLoss();
+    }
+
+}
